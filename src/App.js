6ebb61e132892d5703b295f7c3b0902c79d617a3
zhaojing1987 <jing.zhao@websoft9.com>
2023-07-14 17:02:32 +0800

    update

--- src/App.js
+++ src/App.js
@@ -8,53 +8,62 @@ import "./App.css";
 
 function App() {
   const [iframeSrc, setIframeSrc] = useState(null);
-  const [showAlert, setShowAlert] = useState(false); //用于是否显示错误提示
-  const [alertMessage, setAlertMessage] = useState("");//用于显示错误提示消息
+  const [showAlert, setShowAlert] = useState(false);
+  const [iframeKey, setIframeKey] = useState(Math.random());
+  const [alertMessage, setAlertMessage] = useState("");
 
   var baseURL;
 
   const getData = async () => {
     let protocol = window.location.protocol;
     let host = window.location.host;
+    let portainer_jwt = window.localStorage.getItem("portainer.JWT");
     baseURL = protocol + "//" + (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(host) ? host.split(":")[0] : host);
 
-    cockpit.spawn(["docker", "inspect", "-f", "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "websoft9-appmanage"])
-      .then(function (data) {
+    try {
+      if (!portainer_jwt) {
+        let data = await cockpit.spawn(["docker", "inspect", "-f", "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "websoft9-appmanage"]);
         let IP = data.trim();
         if (IP) {
-          cockpit.http({ "address": IP, "port": 5000 })
-            .get("/AppSearchUsers", { "plugin_name": "portainer" }).then(function (response) {
-              response = JSON.parse(response);
-              if (response.ResponseData) {
-                var userName = response.ResponseData.user?.user_name;
-                var userPwd = response.ResponseData.user?.password;
+          let response = await cockpit.http({ "address": IP, "port": 5000 }).get("/AppSearchUsers", { "plugin_name": "portainer" });
+          response = JSON.parse(response);
+          if (response.ResponseData) {
+            var userName = response.ResponseData.user?.user_name;
+            var userPwd = response.ResponseData.user?.password;
 
-                axios.post(baseURL + "/portainer/api/auth", {
-                  username: userName,
-                  password: userPwd,
-                }).then(authResponse => {
-                  if (authResponse.status === 200) {
-                    setIframeSrc(baseURL + "/portainer?portainer_jwt=" + authResponse.data.jwt);
-                  } else {
-                    setShowAlert(true);
-                    setAlertMessage("Auth Portainer Error.")
-                  }
-                }).catch(error => {
-                  setShowAlert(true);
-                  setAlertMessage("Call Portainer API Error.")
-                });
-              }
-            }).catch((error) => {
-              setShowAlert(true);
-              setAlertMessage("Get Portainer User Error.")
+            const authResponse = await axios.post(baseURL + "/portainer/api/auth", {
+              username: userName,
+              password: userPwd,
             });
+            if (authResponse.status === 200) {
+              let jwt = "\"" + authResponse.data.jwt + "\"";
+              window.localStorage.setItem('portainer\.JWT', jwt);
+            } else {
+              setShowAlert(true);
+              setAlertMessage("Auth Portainer Error.")
+            }
+          }
         }
-      })
-      .catch((error) => {
-        setShowAlert(true);
-        setAlertMessage("Call websoft9-appmanage IP Error.")
-      });
-  };
+      }
+      setIframeKey(Math.random());
+      var newHash = window.location.hash;
+      if (newHash.includes("/portainer/#!/")) {
+        var index = newHash.indexOf("#");
+        if (index > -1) {
+          var content = newHash.slice(index + 1);
+          setIframeKey(Math.random());
+          setIframeSrc(baseURL + content + "?portainer_jwt=" + portainer_jwt.replace(/"/g, ''));
+        }
+      }
+      else {
+        setIframeSrc(baseURL + "/portainer?portainer_jwt=" + portainer_jwt.replace(/"/g, ''));
+      }
+    }
+    catch (error) {
+      setShowAlert(true);
+      setAlertMessage("Call Portainer Page Error.")
+    }
+  }
 
   const handleHashChange = () => {
     var newHash = window.location.hash;
@@ -62,13 +71,15 @@ function App() {
       var index = newHash.indexOf("#");
       if (index > -1) {
         var content = newHash.slice(index + 1);
+        setIframeKey(Math.random());
         setIframeSrc(baseURL + content);
       }
     }
   }
 
-  useEffect(() => {
-    getData();
+  useEffect(async () => {
+    await getData();
+
     window.addEventListener("hashchange", handleHashChange, true);
     return () => {
       window.removeEventListener("hashchange", handleHashChange, true);
@@ -79,7 +90,7 @@ function App() {
     <>
       {iframeSrc ? (
         <div class="myPortainer">
-          <iframe id="myPortainer" title="portainer" src={iframeSrc} />
+          <iframe key={iframeKey} title="portainer" src={iframeSrc} />
         </div>
       ) : (
         <div className="d-flex align-items-center justify-content-center m-5" style={{ flexDirection: "column" }}>
