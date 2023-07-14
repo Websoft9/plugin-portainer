dd76c5a923a9f0e24d24cb9eada01e16df62262e
zhaojing1987 <jing.zhao@websoft9.com>
2023-07-14 09:21:15 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import axios from "axios";
+import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
+import cockpit from 'cockpit';
 import { useEffect, useState } from "react";
-import Spinner from "react-bootstrap/Spinner";
+import { Alert } from 'react-bootstrap';
+import Spinner from 'react-bootstrap/Spinner';
 import "./App.css";
 
 function App() {
-  const [portainerJWT, setPortainerJWT] = useState(null);
-  const [portainerHomePage, setPortainerHomePage] = useState(null);
   const [iframeSrc, setIframeSrc] = useState(null);
+  const [showAlert, setShowAlert] = useState(false); //用于是否显示错误提示
+  const [alertMessage, setAlertMessage] = useState("");//用于显示错误提示消息
+
+  var baseURL;
 
   const getData = async () => {
-    try {
-      let jwt = window.localStorage.getItem("portainer.JWT"); //获取存储在本地的JWT数据
+    let protocol = window.location.protocol;
+    let host = window.location.host;
+    baseURL = protocol + "//" + (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(host) ? host.split(":")[0] : host);
 
-      if (jwt === null) {
-        const response = await axios.get("../myapps/config.json"); //从项目下读取配置文件
-        if (response.status === 200) {
-          let config = response.data.PORTAINER;
-          const {
-            PORTAINER_USERNAME,
-            PORTAINER_PASSWORD,
-            PORTAINER_AUTH_URL,
-            PORTAINER_HOME_PAGE,
-          } = config;
-          setPortainerHomePage(PORTAINER_HOME_PAGE);
+    cockpit.spawn(["docker", "inspect", "-f", "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "websoft9-appmanage"])
+      .then(function (data) {
+        let IP = data.trim();
+        if (IP) {
+          cockpit.http({ "address": IP, "port": 5000 })
+            .get("/AppSearchUsers", { "plugin_name": "portainer" }).then(function (response) {
+              response = JSON.parse(response);
+              if (response.ResponseData) {
+                var userName = response.ResponseData.user?.user_name;
+                var userPwd = response.ResponseData.user?.password;
 
-          //调用portainer的登录API，模拟登录
-          const authResponse = await axios.post(PORTAINER_AUTH_URL, {
-            username: PORTAINER_USERNAME,
-            password: PORTAINER_PASSWORD,
-          });
-          if (authResponse.status === 200) {
-            jwt = '"' + authResponse.data.jwt + '"';
-            setPortainerJWT(jwt);
-            window.localStorage.setItem("portainer.JWT", jwt); //关键是将通过API登录后获取的jwt，存储到本地localStorage
-          } else {
-            console.log("Error:", authResponse);
-          }
-        } else {
-          console.log("Error:", response);
+                axios.post(baseURL + "/portainer/api/auth", {
+                  username: userName,
+                  password: userPwd,
+                }).then(authResponse => {
+                  if (authResponse.status === 200) {
+                    setIframeSrc(baseURL + "/portainer?portainer_jwt=" + authResponse.data.jwt);
+                  } else {
+                    setShowAlert(true);
+                    setAlertMessage("Auth Portainer Error.")
+                  }
+                }).catch(error => {
+                  setShowAlert(true);
+                  setAlertMessage("Call Portainer API Error.")
+                });
+              }
+            }).catch((error) => {
+              setShowAlert(true);
+              setAlertMessage("Get Portainer User Error.")
+            });
         }
-      } else {
-        const response = await axios.get("../myapps/config.json"); //从项目下读取配置文件
-        if (response.status === 200) {
-          let config = response.data.PORTAINER;
-          const { PORTAINER_HOME_PAGE } = config;
-          setPortainerHomePage(PORTAINER_HOME_PAGE);
-        } else {
-          console.log("Error:", response);
-        }
-        setPortainerJWT(jwt);
-      }
-    } catch (error) {
-      console.log("Error:", error);
-    }
+      })
+      .catch((error) => {
+        setShowAlert(true);
+        setAlertMessage("Call websoft9-appmanage IP Error.")
+      });
   };
 
   function handleHashChange() {
@@ -62,24 +62,13 @@ function App() {
       var index = newHash.indexOf("#");
       if (index > -1) {
         var content = newHash.slice(index + 1);
-        setIframeSrc(content);
+        setIframeSrc(baseURL + content);
       }
     }
   }
 
   useEffect(() => {
     getData();
-    let currentHash = window.location.hash;
-    if (currentHash.includes("/portainer/#!/")) {
-      var index = currentHash.indexOf("#");
-      if (index > -1) {
-        var content = currentHash.slice(index + 1);
-        setIframeSrc(content);
-      }
-    }
-    else {
-      setIframeSrc(portainerHomePage);
-    }
     window.addEventListener("hashchange", handleHashChange, true);
     return () => {
       window.removeEventListener("hashchange", handleHashChange, true);
@@ -88,13 +77,16 @@ function App() {
 
   return (
     <>
-      {portainerJWT && portainerHomePage ? (
+      {iframeSrc ? (
         <div class="myPortainer">
-          <iframe id="myPortainer" title="portainer" src={iframeSrc || portainerHomePage} />
+          <iframe id="myPortainer" title="portainer" src={iframeSrc} />
         </div>
       ) : (
-        <div className="d-flex align-items-center justify-content-center m-5">
-          <Spinner animation="border" variant="secondary" />
+        <div className="d-flex align-items-center justify-content-center m-5" style={{ flexDirection: "column" }}>
+          <Spinner animation="border" variant="secondary" className='mb-5' />
+          {showAlert && <Alert variant="danger" className="my-2">
+            {alertMessage}
+          </Alert>}
         </div>
       )}
     </>
