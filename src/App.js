29f1336b3b0994a33b73f2d9fccd23f492b6ec06
zhaojing1987 <jing.zhao@websoft9.com>
2023-08-12 15:10:06 +0800

    update the portainer's autologin tyoe

--- src/App.js
+++ src/App.js
@@ -15,8 +15,18 @@ function App() {
 
   const host = window.location.host;
   const baseURL = window.location.protocol + "//" + (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(host) ? host.split(":")[0] : host);
-  //let portainer_jwt = window.localStorage.getItem("portainer.JWT"); //获取portainer.JWT的值
-  let portainer_jwt = window.sessionStorage.getItem("portainerJWT"); //获取portainerJWT的值
+
+  //获取cookie
+  function getCookieValue(cookieName) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name === cookieName) {
+        return decodeURIComponent(value);
+      }
+    }
+    return null; // 如果没有找到该 Cookie 返回 null
+  }
 
   //验证JWT是否过期
   function isTokenExpired(token) {
@@ -38,10 +48,8 @@ function App() {
         password: userPwd,
       });
       if (authResponse.status === 200) {
-        //portainer_jwt = "\"" + authResponse.data.jwt + "\"";
-        portainer_jwt = authResponse.data.jwt;
-        //window.localStorage.setItem('portainer\.JWT', portainer_jwt);
-        window.sessionStorage.setItem('portainerJWT', portainer_jwt); //存储数据
+        const portainer_jwt = authResponse.data.jwt;
+        document.cookie = "portainerJWT=" + portainer_jwt + ";path=/";
       } else {
         setShowAlert(true);
         setAlertMessage("Request Portainer JWT Failed.");
@@ -51,11 +59,12 @@ function App() {
 
   const getData = async () => {
     try {
-      if (!portainer_jwt) { //如果不存在，通过API获取JWT
+      const jwtToken = getCookieValue("portainerJWT");
+      if (!jwtToken) {
         await getJwt();
       }
-      else { //如果存在，就验证是否已经过期
-        const isExpired = isTokenExpired(portainer_jwt);
+      else {
+        const isExpired = isTokenExpired(jwtToken);
         if (isExpired) { //如果已经过期，重新生成JWT
           await getJwt();
         }
@@ -67,11 +76,11 @@ function App() {
         var index = newHash.indexOf("#");
         if (index > -1) {
           var content = newHash.slice(index + 1);
-          setIframeSrc(baseURL + content + "?portainer_jwt=" + portainer_jwt);
+          setIframeSrc(baseURL + content);
         }
       }
       else {
-        setIframeSrc(baseURL + "/portainer?portainer_jwt=" + portainer_jwt);
+        setIframeSrc(baseURL + "/portainer");
       }
     }
     catch (error) {
@@ -87,7 +96,7 @@ function App() {
       if (index > -1) {
         var content = newHash.slice(index + 1);
         setIframeKey(Math.random());
-        setIframeSrc(baseURL + content + "?portainer_jwt=" + portainer_jwt);
+        setIframeSrc(baseURL + content);
       }
     }
   }
